42dee9494c1b66036a610ac6681977b79ebac99c
U08410006 <55177753+U08410006@users.noreply.github.com>
2021-08-12 14:24:16 +0800

    add ver3

---
+++ sub_ver3.py
@@ -0,0 +1,39 @@
+# main file of ocr 
+import cv2
+import easyocr
+import sys
+
+f = open('output_ver3.txt', 'w') # output file
+reader = easyocr.Reader(['ch_tra']) # use chinese 
+
+x = 0 # x-axis of picture start
+y = 580 # y-axis of picture start
+
+w = 1120 # width of pic
+h = 150 # height of pic
+
+for i in range(1000): 
+ 
+    path = 'pic/out' + str(i+1) + '.png'
+    img = cv2.imread(path)
+
+    crop_img = img[y:y+h, x:x+w]
+    cv2.imwrite('output.png', crop_img) # save img to read
+
+    result = reader.readtext('output.png')
+    l = len(result)
+
+    for j in range(l):
+        if((abs(result[j][0][0][1] - 8) <= 8) or (abs(result[j][0][0][1] - 37) <= 8) or (abs(result[j][0][0][1] - 28) <= 8 )):
+            f.write(result[j][1])
+            f.write(" ")
+        elif (abs(result[j][0][0][1] - 56) <= 8):
+            f.write("&")
+            f.write(result[j][1])
+            f.write(" ")
+        else:
+            f.write("#")
+            f.write(result[j][1])
+            f.write(" ")
+    f.write('\n')
+
---
+++ vote_ver3.py
@@ -0,0 +1,65 @@
+import distance
+
+def sec2time(t):
+    int_m = int(t/600)
+    float_s = format(((t - ( int_m * 600 )) / 10), '.1f')
+    time_str = str(int_m) + " : " + str(float_s)
+    return time_str
+
+input_file = open('output.txt', 'r', encoding='utf-8')
+output_file = open('final_ver3.txt', 'w', encoding='utf-8')
+lines = input_file.readlines()
+input_file.close()
+
+num = ['0','1','2','3','4','5','6','7','8','9',' ','\n','`','.','!',',','_'] # the words can't have nums
+
+left_block = [""]
+right_block = [""]
+line_cnt = 0
+line_cnt_last = 0
+left_last = ""
+right_last = ""
+for line in lines:
+    line_cnt += 1
+    illegal = line.find('#')
+    if(illegal == -1):
+        regular = line
+    elif(illegal == 0):
+        regular = ""
+    else:
+        regular = line[0 : illegal - 1]
+    content = regular.split('&') # spilt it into two part
+    if(len(content) > 3): # there are more than two parts
+        print("noise error: ",line_cnt)
+        continue
+    elif(len(content) == 2 ):  # there just one part
+        content[1] = content[0]
+    elif(len(content) == 1): # there is no content
+        content[0] = ""
+        content.append("")
+    left_now = content[0]
+    right_now = content[1]
+
+    for i in num:
+        left_now = left_now.replace(i,'')
+        right_now = right_now.replace(i,'')
+    dis_left = distance.levenshtein(left_now,left_last)
+    dis_right = distance.levenshtein(right_now,right_last)
+    if(dis_left >= 2 and dis_right >= 2):
+        left_most = max(set(left_block), key=left_block.count)
+        right_most = max(set(right_block), key=right_block.count)
+        output_file.write("台 : " + left_most + "，國 : " + right_most + "，時間 : " + sec2time(line_cnt_last) + " ~ " + sec2time(line_cnt) + '\n')
+        line_cnt_last = line_cnt
+        left_block.clear()
+        right_block.clear()
+        left_block.append(content[0])
+        right_block.append(content[1])
+    else:
+        left_block.append(content[0])
+        right_block.append(content[1])
+    left_last = left_now
+    right_last = right_now
+
+    
+    
+ 
